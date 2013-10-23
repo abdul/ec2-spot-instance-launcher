2e2661e8cfb03a98e93d5518b068bc8c82647e2c
Abdul Qabiz <abdul.qabiz@gmail.com>
2013-10-23 15:25:16 +0530

    Update code
    - Remove proxifier related stuff
    - Add user_data configuration and code
    - Change config file name
    - Update README

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import subprocess
 import ConfigParser, os, socket
 import sys
 
-config = ConfigParser.ConfigParser()
-config.read('spot-ec2-proxifier.ini')
+def read_user_data_from_local_config():
+        user_data = config.get('EC2', 'user_data')
+        if config.get('EC2', 'user_data') is None or user_data == '':
+                try:
+                        user_data = (open(config.get('EC2', 'user_data_file'), 'r')).read()
+                except:
+                        user_data = ''
+        return user_data
 
 def create_client():
         client = EC2Connection(config.get('IAM', 'access'), config.get('IAM', 'secret'))
@@ -22,16 +28,24 @@ def get_existing_instance(client):
                 return instances[0].instances[0]
         else:
                 return None
+def list_all_existing_instances(client):
+        reservations = client.get_all_instances(filters = { 'tag-value': config.get('EC2', 'tag') })
+        if len(reservations) > 0:
+                r_instances = [inst for resv in reservations for inst in resv.instances]
+                for inst in r_instances:
+                        print "Instance Id: %s" % inst.id
+
 
 def get_spot_price(client):
         price_history = client.get_spot_price_history(instance_type = config.get('EC2', 'type'), product_description = 'Linux/UNIX')
         return price_history[0].price
 
-def provision_instance(client):
+def provision_instance(client, user_data):
         req = client.request_spot_instances(price = config.get('EC2', 'max_bid'), 
                 image_id = config.get('EC2', 'ami'), 
                 instance_type = config.get('EC2', 'type'), 
                 key_name = config.get('EC2', 'key_pair'), 
+                user_data = user_data,
                 security_groups = [config.get('EC2', 'security_group')])[0]
         print 'Spot request created, status: ' + req.state
         print 'Waiting for spot provisioning',
@@ -55,7 +69,7 @@ def destroy_instance(client, inst):
                 print 'Failed to terminate:', sys.exc_info()[0]
 
 def wait_for_up(client, inst):
-        print 'Waiting for instance to come up',
+        print 'Waiting for instance to come up'
         while True:
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 if inst.ip_address is None:
@@ -67,42 +81,42 @@ def wait_for_up(client, inst):
                                 s.connect((inst.ip_address, 22))
                                 s.shutdown(2)
                                 print 'Server is up!'
+                                print 'Server Public IP - %s' % inst.ip_address
                                 break
                 except:
                         print '.',
                 sleep(10)
-        
-def start_plink(inst):
-        plink_cmd = 'plink -N -D ' + config.get('Proxy', 'bind_port') + ' -i ' + config.get('Proxy', 'key_file') + ' ec2-user@' + inst.ip_address
-        print 'Running: ' + plink_cmd
-        print 'Once connection is established, use Ctrl-C to kill the tunnel'
-        plink = subprocess.call(plink_cmd, shell = False)
 
-# Entry
-action = 'start' if len(sys.argv) == 1 else sys.argv[1]
+def main():
+        # Entry
+        action = 'start' if len(sys.argv) == 1 else sys.argv[1]
+        client = create_client()
+        if client is None:
+                print 'Unable to create EC2 client'
+                sys.exit(0)
+        inst = get_existing_instance(client)
+        user_data = read_user_data_from_local_config()
+        if action == 'start':
+                if inst is None:
+                        spot_price = get_spot_price(client)
+                        print 'Spot price is ' + str(spot_price) + ' ...',
+                        if spot_price > float(config.get('EC2', 'max_bid')):
+                                print 'too high!'
+                                sys.exit(0)
+                        else:
+                                print 'below maximum bid, continuing'
+                                provision_instance(client, user_data)
+                                inst = get_existing_instance(client)
+                wait_for_up(client, inst)
+        elif action == 'stop' and inst is not None:
+                destroy_instance(client, inst)
+        elif action == 'list':
+                print 'Active Spot Instnaces (AMI: %s)' % config.get('EC2', 'ami')
+                list_all_existing_instances(client)
+        else:
+                print 'No action taken'
 
-client = create_client()
-if client is None:
-        print 'Unable to create EC2 client'
-        sys.exit(0)
-
-inst = get_existing_instance(client)
-
-if action == 'start':
-        if inst is None:
-                spot_price = get_spot_price(client)
-                print 'Spot price is ' + str(spot_price) + ' ...',
-                if spot_price > float(config.get('EC2', 'max_bid')):
-                        print 'too high!'
-                        sys.exit(0)
-                else:
-                        print 'below maximum bid, continuing'
-                        provision_instance(client)
-                        inst = get_existing_instance(client)
-
-        wait_for_up(client, inst)
-        start_plink(inst)
-elif action == 'stop' and inst is not None:
-        destroy_instance(client, inst)
-else:
-        print 'No action taken'
+if __name__ == "__main__":
+        config = ConfigParser.ConfigParser()
+        config.read('ec2-spot-instance-launcher.cfg')
+        main()
